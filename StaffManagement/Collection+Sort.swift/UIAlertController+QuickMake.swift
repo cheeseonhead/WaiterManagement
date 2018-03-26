69ad875c84f8803d8da1ba7ff7250360f0e0b5b1
Jeffrey Wu <cheeseonhead@gmail.com>
2018-03-26 12:49:27 -0400

    Quick create with textfield

--- StaffManagement/Collection+Sort.swift/UIAlertController+QuickMake.swift
+++ StaffManagement/Collection+Sort.swift/UIAlertController+QuickMake.swift
@@ -10,11 +10,19 @@ import Foundation
 import UIKit
 
 extension UIAlertController {
-    public class func singleInputAlert(title: String, message: String, confirmAction: String, handler: ((UIAlertAction) -> Void)?) -> UIAlertController {
+    public class func singleInputAlert(title: String, message: String, confirmTitle: String, placeHolder: String?, handler: ((String) -> Void)?) -> UIAlertController {
         let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
+        ac.addTextField { (textfield) in
+            textfield.placeholder = placeHolder
+        }
+        
         let cancel = UIAlertAction(title: NSLocalizedString("Cancel", comment: "Alert"), style: .cancel, handler: nil)
-        let submit = UIAlertAction(title: NSLocalizedString(confirmAction, comment: "Alert"), style: .default, handler: handler)
+        let submit = UIAlertAction(title: NSLocalizedString(confirmTitle, comment: "Alert"), style: .default) {
+            _ in
+            let input = ac.textFields?.first?.text ?? ""
+            handler?(input)
+        }
         
         ac.addAction(cancel)
         ac.addAction(submit)
