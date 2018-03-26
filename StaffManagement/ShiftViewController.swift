2f45bacd6ee3eb0a7bfee0434026b27780b8b677
Jeffrey Wu <cheeseonhead@gmail.com>
2018-03-26 15:59:37 -0400

    Added shift view controller

---
+++ StaffManagement/ShiftViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ShiftViewController.swift
+//  StaffManagement
+//
+//  Created by Jeffrey Wu on 2018-03-26.
+//  Copyright © 2018 Derek Harasen. All rights reserved.
+//
+
+import Foundation
